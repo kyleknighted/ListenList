b7427630f41d194f6c7514264bd20a764181ce76
Kyle Knight <kyleaknight@gmail.com>
2012-07-31 14:25:31 -0500

    moving over the arist_name to models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,7 @@
 class Album < ActiveRecord::Base
+  belongs_to :user
+
+  def has_artist?
+    defined?(self.first.artist_name)
+  end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,7 @@
 class Track < ActiveRecord::Base
+  belongs_to :user
+
+  def has_artist?
+    defined?(self.first.artist_name)
+  end
 end
